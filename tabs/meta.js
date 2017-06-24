84a9cd7b6b11476a7404dedd6d616cada81d9f89
李光耀 <liguangyao@liguangyaodeiMac.local>
2017-06-24 12:58:48 +0800

    修改

--- tabs/meta.js
+++ tabs/meta.js
@@ -1,20 +1,21 @@
 module.exports = {
   "prompts": {
     "name": {
-      type: 'input',
-      name: 'favorite',
-      message: 'Bacon lover, what is your favorite type of bacon?',
+      "type": "input",
+      "required": false,
+      "message": "Act file name",
+      "default": "demo name"
     },
     "description": {
-      type: "input",
-      required: false,
-      message: "Project description",
-      default: "A act page template"
+      "type": "input",
+      "required": false,
+      "message": "Project description",
+      "default": "A act page template"
     },
     "downloadPrompte": {
-      type: "confirm",
-      message: "是否需要下载app的提示",
-      default: true
+      "type": "confirm",
+      "message": "是否需要下载app的提示",
+      "default": true
     },
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
