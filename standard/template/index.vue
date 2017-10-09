c6bc8e9e59d0c25c3db569011624a0f816f6a9c5
苏忠辉 <suzhonghui@xwowmall.com>
2017-10-09 11:27:19 +0800

    更新了模板

--- standard/template/index.vue
+++ standard/template/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class='container' style='font-size:24px'>
-    <header style='background:#ff4e00;color:#fff;height:100px;line-height:100px;text-align:center'>
+    <header>
       标准模板
     </header>
     {{#if_eq downloadPrompte true}}
@@ -52,22 +52,27 @@ export default {
   },
   mounted() {
     this.$setDocumentTitle(this.shareText.title)
-    this.$setShareInfo(
-      this.shareText.shareTitle,
-      this.shareText.shareSubTitle,
-      this.shareText.shareImg,
-      this.shareText.shareLink
+    const title = this.shareText.shareTitle
+    const subtitle = this.shareText.shareSubTitle
+    const thumbnail = this.shareText.shareImg
+    const url = this.shareText.shareLink
+    this.$initShareInfo(
+      title,
+      subtitle,
+      thumbnail,
+      url,
     )
   },
 }
 </script>
 <style lang='scss' scoped>
+  @import '~assets/scss/common/index';
   *,*:before,*:after{
     box-sizing: border-box;
   }
   .fold{
     width: 100%;
-    height: 88px;
+    height: px2rem(88);
   }
   .fold-enter-active, .fold-leave-active {
     transition: all .5s;
@@ -75,4 +80,13 @@ export default {
   .fold-enter, .fold-leave-to /* .fade-leave-active in below version 2.1.8 */ {
     height: 0;
   }  
+
+  // 无用代码
+  header {
+    background:#ff4e00;
+    color:#fff;
+    height:100px;
+    line-height:100px;
+    text-align:center;
+  }
 </style>
